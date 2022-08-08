b0064a93f96a925015268a409dcf0404c533012c
StarGrim33 <prosky33@gmail.com>
2022-08-08 12:26:52 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace JuniorHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "There is something about us i want to say";
+
+            string[] array = text.Split();
+
+            foreach(string split in array)
+            {
+                Console.WriteLine(split);
+            }
+
+        }
+    }
+}
